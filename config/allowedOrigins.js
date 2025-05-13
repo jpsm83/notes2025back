c8fe3482196749fb969c77810d6026884e06b445
jpsm83 <73789298+jpsm83@users.noreply.github.com>
2025-05-13 19:55:55 +0200

    for the love of god

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 // all allowed origins for CORS requests
 const allowedOrigins = [
   "https://notes2025front-eft6ywww5-jpsm83s-projects.vercel.app",
-  "https://notes2025front.vercel.app/",
+  "https://notes2025front.vercel.app",
 ];
 
 if (process.env.NODE_ENV === "development") {
